f50e4309685e8136db54015d1be3d4fc66045d9d
Aurélien Hervé <mail@aurelien-herve.com>
2019-11-24 19:14:46 +0100

    terraform init

---
+++ environments/production/main.tf
@@ -0,0 +1,27 @@
+locals {
+  project     = "go-lambda-api"
+  environment = "production"
+  region      = "eu-west-1"
+}
+
+terraform {
+  backend s3 {
+    bucket = "aherve-terraform"
+    key    = "go-lambda-api/terraform.tfstate"
+    region = "eu-west-1"
+  }
+}
+
+provider aws {
+  alias   = "ire"
+  region  = "${local.region}"
+  version = "~> 2.30"
+}
+
+module main {
+  source = "../../"
+
+  providers = {
+    "aws" = "aws.ire"
+  }
+}
